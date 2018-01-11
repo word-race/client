056abd3459165b4c59676c5823d6e00f17c1c8f1
wahibhanii <wahib.hanii@gmail.com>
2018-01-11 18:15:51 +0700

    create racepage layout

--- src/components/RacePage.vue
+++ src/components/RacePage.vue
@@ -1,5 +1,25 @@
 <template>
-  <h1>Race Page</h1>
+  <v-container>
+    <h1>Race Page</h1>
+    <v-layout>
+      <v-flex>Indicator</v-flex>
+      <v-flex>WORD</v-flex>
+      <v-flex>Time lieft</v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex>Your Point</v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex>Player Name</v-flex>
+      <v-flex>Player type board</v-flex>
+      <v-flex>Ready Button</v-flex>
+    </v-layout>
+    <v-layout>
+      Other player board component
+    </v-layout>
+  </v-container>
+  
+
 </template>
 
 <script>
